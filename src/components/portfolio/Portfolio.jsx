b656bba3e6f70b9209f85fda110c8e56c740a16a
ELyakhloufimohammedamine <125660459+ELyakhloufimohammedamine@users.noreply.github.com>
2024-02-27 19:19:40 +0100

    Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,56 +6,56 @@ const items =[
     {
         id:1,
         title:"E-Commerce  Django",
-        img:"public/ecom.png",
+        img:"/ecom.png",
         desc:" Projet E-Commerce avec Django, offrant une plateforme conviviale pour la vente de produits traditionnels marocains",
 
     },
     {
         id:2,
         title:"Harvard CS50 Certificate",
-        img:"public/CS50x (1).png",
+        img:"/CS50x (1).png",
         desc:" I have successfully completed the CS50x course offered by Harvard University through edX and have earned my certificate. ",
 
     },
     {
         id:3,
         title:" web app  to buy and sell stocks",
-        img:"public/WhatsApp Image 2024-02-27 at 13.42.43.jpeg",
+        img:"/WhatsApp Image 2024-02-27 at 13.42.43.jpeg",
         desc:" web application that allows users to buy and sell stocks, and view their transaction history and current portfolio value I used Flask, SQL, HTML, CSS, Bootstrap",
 
     },
     {
         id:4,
         title:" App pour gérer les informations personnelles",
-        img:"public/WhatsApp Image 2024-02-27 at 13.25.13.jpeg",
+        img:"/WhatsApp Image 2024-02-27 at 13.25.13.jpeg",
         desc:"  utilisant python Tkinter Cette application stocke ces informations dans une base de données en utilisant SQLite3.",
 
     },
     {
         id:5,
         title:"Aws cloud Practitioner Essentials",
-        img:"public/1666016829132.jpg",
+        img:"/1666016829132.jpg",
         desc:" La certification vise à valider la compréhension générale du cloud AWS, y compris les concepts de base, les services AWS principaux, l'architecture cloud et les meilleures pratiques de sécurité.",
 
     },
     {
         id:6,
         title:"Data Analytics Professional Certificate",
-        img:"public/google-data-analytics-professional-certificate.2.png",
+        img:"/google-data-analytics-professional-certificate.2.png",
         desc:" La certification Google Data Analytics Professional Certificate est une formation complète conçue pour permettre aux apprenants de développer des compétences essentielles en analyse de données",
 
     },
     {
         id:7,
         title:"Meta React Basics certificat",
-        img:"public/Capture d’écran 2024-02-27 135003.jpg",
+        img:"/Capture d’écran 2024-02-27 135003.jpg",
         desc:"  Le cours React Basics couvre les bases de React, y compris la mise en place d'un environnement de développement, la création de composants React, la gestion de l'état, la gestion des événements, et la navigation entre les composants.",
 
     },
     {
       id:8,
       title:"Gase center website",
-      img:"public/Capture d’écran 2024-02-27 160131.jpg",
+      img:"/Capture d’écran 2024-02-27 160131.jpg",
       desc:" un site de présentation pour une entreprise de livraison de gaz à Kiel et ses environs. avec Wordpress",
 
   },
